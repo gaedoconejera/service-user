8ffbd252c17c12e618893623d1bb9d4a42b76c42
gaedo.vrgroup <gaedo.vrgroup@bicevida.cl>
2024-10-15 02:59:37 -0300

    Servidor de registro para arquitectura de microservicios

---
+++ springboot-servicio-eureka-server/src/test/java/cl/aedo/springboot/app/eureka/SpringbootServicioEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.aedo.springboot.app.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
